af4b7841b9a6127306fb4557a8500532535827c7
StarrFox <starrfox6312@gmail.com>
2022-09-28 11:49:50 -0700

    add more test cases for pad_format

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,3 +4,5 @@ from memobj import utils
 def test_pad_format():
     assert utils.pad_format("<bib") == "<bxxxibxxx"
     assert utils.pad_format("<bqb") == "<bxxxxxxxqbxxxxxxx"
+    assert utils.pad_format("<lhh") == "<lhh"
+    assert utils.pad_format("<clh") == "<cxxxlhxx"
