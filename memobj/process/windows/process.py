c11e052fc1f34df6937f759b554e388fe870a228
StarrFox <StarrFox6312@gmail.com>
2024-02-08 11:44:15 -0800

    move process to a file and only create ctypes structs once

--- memobj/process/windows/process.py
+++ memobj/process/windows/process.py
@@ -1,281 +1,24 @@
 import ctypes
 import ctypes.wintypes
-import enum
 import functools
-import platform
-import struct
 import typing
-from typing import Any, Union, Self
 from pathlib import Path
+from typing import Self, Union
 
-# faster than builtin re
 import regex
 
-
-class Process:
-    """A connected process"""
-
-    @functools.cached_property
-    def process_64_bit(self) -> bool:
-        """
-        If this process is 64 bit
-        """
-        raise NotImplementedError()
-
-    @functools.cached_property
-    def python_64_bit(self) -> bool:
-        """
-        If the current python is 64 bit
-        """
-        # we can just check the pointer size; 4 = 32, 8 = 64
-        return ctypes.sizeof(ctypes.c_void_p) == 8
-
-    @functools.cached_property
-    def system_64_bit(self) -> bool:
-        """
-        If the system is 64 bit
-        """
-        return platform.architecture()[0] == "64bit"
-
-    @functools.cached_property
-    def executable_path(self) -> Path:
-        """
-        Path to the process's executable
-        """
-        raise NotImplementedError()
-
-    @functools.cached_property
-    def pointer_format_string(self) -> str:
-        if self.process_64_bit:
-            return "Q"
-        else:
-            return "I"
-
-    @functools.cached_property
-    def pointer_size(self) -> int:
-        if self.process_64_bit:
-            return 8
-        else:
-            return 4
-
-    @classmethod
-    def from_name(cls, name: str) -> Self:
-        """
-        Open a process by name
-
-        Args:
-            name: The name of the process to open
-
-        Returns:
-        The opened process
-        """
-        raise NotImplementedError()
-
-    @classmethod
-    def from_id(cls, pid: int) -> Self:
-        """
-        Open a process by id
-
-        Args:
-            pid: The id of the process to open
-
-        Returns:
-        The opened process
-        """
-        raise NotImplementedError()
-
-    def allocate_memory(self, size: int) -> int:
-        """
-        Allocate <size> amount of memory in the process
-
-        Args:
-            size: The amount of memory to allocate
-
-        Returns:
-        The start address of the allocated memory
-        """
-        raise NotImplementedError()
-
-    def free_memory(self, address: int):
-        """
-        Free some memory in the process
-
-        Args:
-            address: The start address of the area to free
-        """
-        raise NotImplementedError()
-
-    def read_memory(self, address: int, size: int) -> bytes:
-        """
-        Read bytes from memory
-
-        Args:
-            address: The address to read from
-            size: The number of bytes to read
-
-        Returns:
-        The bytes read
-        """
-        raise NotImplementedError()
-
-    def write_memory(self, address: int, value: bytes):
-        """
-        Write bytes to memory
-
-        Args:
-            address: The address to write to
-            value: The bytes to write to that address
-        """
-        raise NotImplementedError()
-
-    def scan_memory(self, pattern: regex.Pattern | bytes, *, module: str | None = None) -> list[int]:
-        """
-        Scan memory for a regex pattern
-
-        Args:
-            pattern: A regex.Pattern or a byte pattern
-            module: Name of a module to exclusively search
-
-        Returns:
-        A list of addresses that matched
-        """
-        raise NotImplementedError()
-
-    def scan_one(self, pattern: regex.Pattern | bytes, *, module: str | None = None) -> int:
-        """
-        Scan memory for a regex pattern and error if one address was not found
-
-        Args:
-            pattern: A regex.Pattern or a byte pattern
-            module: Name of a module to exclusively search
-
-        Returns:
-        Address found
-        """
-        results = self.scan_memory(pattern, module=module)
-        
-        if result_len := len(results) == 0:
-            raise ValueError(F"No matches found for pattern {pattern}")
-        
-        elif result_len > 1:
-            raise ValueError(f"Multiple matches found for pattern {pattern}")
-        
-        return results[0]
-
-    def read_formatted(self, address: int, format_string: str) -> tuple[Any] | Any:
-        """
-        Read formatted bytes from memory, format_string is passed directly to struct.unpack
-
-        Args:
-            address: The address to read from
-            format_string: The format string to pass to struct.unpack
-
-        Returns:
-        The formatted data (the corresponding python type/tuple of)
-        """
-        raw_data = self.read_memory(address, struct.calcsize(format_string))
-
-        # struct.unpack is actually faster than int.from_data for some reason
-        formatted = struct.unpack(format_string, raw_data)
-
-        if len(formatted) == 1:
-            return formatted[0]
-
-        return formatted
-
-    def write_formatted(self, address: int, format_string: str, value: tuple[Any] | Any):
-        """
-        Write formatted bytes to memory, format_string is passed directly to struct.pack
-
-        Args:
-            address: The address to write to
-            format_string: The format string to pass to struct.pack
-            value: The data to pass to struct.pack
-        """
-        packed_data = struct.pack(format_string, value)
-        self.write_memory(address, packed_data)
-
-    # TODO: scan_formatted? scan_formatted(format_string, value)
-
-
-class CheckWindowsOsError:
-    def __enter__(self):
-        # https://learn.microsoft.com/en-us/windows/win32/api/errhandlingapi/nf-errhandlingapi-setlasterror
-        ctypes.windll.kernel32.SetLastError(0)
-
-    # TODO: use an exceptiongroup here
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        # https://learn.microsoft.com/en-us/windows/win32/api/errhandlingapi/nf-errhandlingapi-getlasterror
-        last_error = ctypes.windll.kernel32.GetLastError()
-
-        if last_error != 0:
-            raise ctypes.WinError(last_error)
-
-
-# https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-memory_basic_information
-class WindowsMemoryBasicInformation(ctypes.Structure):
-    # TODO: do they mean the debugger should use this type if it's 32 bit or if the process is?
-    #  probably process based
-    # just some casual trolling by windows
-    # see https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-memory_basic_information#remarks
-    if ctypes.sizeof(ctypes.c_void_p) == 8:
-        _fields_ = [
-            ("BaseAddress", ctypes.c_ulonglong),
-            ("AllocationBase", ctypes.c_ulonglong),
-            ("AllocationProtect", ctypes.wintypes.DWORD),
-            ("_alignment1", ctypes.wintypes.DWORD),
-            ("RegionSize", ctypes.c_ulonglong),
-            ("State", ctypes.wintypes.DWORD),
-            ("Protect", ctypes.wintypes.DWORD),
-            ("Type", ctypes.wintypes.DWORD),
-            ("_alignment2", ctypes.wintypes.DWORD),
-        ]
-    else:
-        _fields_ = [
-            ("BaseAddress", ctypes.wintypes.DWORD),
-            ("AllocationBase", ctypes.wintypes.DWORD),
-            ("AllocationProtect", ctypes.wintypes.DWORD),
-            ("RegionSize", ctypes.wintypes.DWORD),
-            ("State", ctypes.wintypes.DWORD),
-            ("Protect", ctypes.wintypes.DWORD),
-            ("Type", ctypes.wintypes.DWORD),
-        ]
-
-
-# https://learn.microsoft.com/en-us/windows/win32/api/psapi/ns-psapi-moduleinfo
-class WindowsModuleInfo(ctypes.Structure):
-    _fields_ = [
-        ("lpBaseOfDll", ctypes.wintypes.LPVOID),
-        ("SizeOfImage", ctypes.wintypes.DWORD),
-        ("EntryPoint", ctypes.wintypes.LPVOID),
-    ]
-
-
-# https://learn.microsoft.com/en-us/windows/win32/memory/memory-protection-constants
-class WindowsMemoryProtection(enum.IntFlag):
-    # note: can't read
-    PAGE_NOACCESS = 0x1
-    # note: can't write
-    PAGE_READONLY = 0x2
-    PAGE_READWRITE = 0x4
-    # note: wacky on write
-    PAGE_WRITECOPY = 0x8
-    # note: can't write, can't read
-    PAGE_EXECUTE = 0x10
-    # note: can't write
-    PAGE_EXECUTE_READ = 0x20
-    PAGE_EXECUTE_READWRITE = 0x40
-    # note: does wacky stuff on write
-    PAGE_EXECUTE_WRITECOPY = 0x80
-    # note: can't read
-    PAGE_GUARD = 0x100
-    # note: what does non-cachable mean
-    PAGE_NOCACHE = 0x200
-    # note: what is this
-    PAGE_WRITECOMBINE = 0x400
-    # note: what if CFG info
-    PAGE_TARGETS_INVALID = 0x40000000
-    PAGE_TARGETS_NO_UPDATE = 0x40000000
+from memobj.process.windows.utils import (
+    CheckWindowsOsError,
+    WindowsModuleInfo,
+    WindowsMemoryProtection,  # TODO: what was this going to be used for?
+    WindowsMemoryBasicInformation,
+    LUID,
+    LUID_AND_ATTRIBUTES,
+    SingleLUIDAndAttributes,
+    TOKEN_PRIVILEGES,
+    PROCESSENTRY32
+)
+from memobj.process import Process
 
 
 class WindowsProcess(Process):
@@ -284,29 +27,6 @@ class WindowsProcess(Process):
 
     @staticmethod
     def _get_debug_privileges():
-        # https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-luid
-        class LUID(ctypes.Structure):
-            _fields_ = [
-                ("LowPart", ctypes.wintypes.DWORD),
-                ("HighPart", ctypes.c_long),
-            ]
-
-        # https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-luid_and_attributes
-        class LUID_AND_ATTRIBUTES(ctypes.Structure):
-            _fields_ = [
-                ("Luid", LUID),
-                ("Attributes", ctypes.wintypes.DWORD),
-            ]
-
-        SingleLUIDAndAttributes = LUID_AND_ATTRIBUTES * 1
-
-        # https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-token_privileges
-        class TOKEN_PRIVILEGES(ctypes.Structure):
-            _fields_ = [
-                ("PrivilegeCount", ctypes.wintypes.DWORD),
-                ("Privileges", SingleLUIDAndAttributes),  # we are only ever specifying one privilege
-            ]
-
         with CheckWindowsOsError():
             # https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getcurrentprocess
             proc = ctypes.windll.kernel32.GetCurrentProcess()
@@ -400,23 +120,6 @@ class WindowsProcess(Process):
 
     @classmethod
     def from_name(cls, name: str, *, require_debug: bool = True) -> Self:
-        import ctypes.wintypes
-
-        # https://learn.microsoft.com/en-us/windows/win32/api/tlhelp32/ns-tlhelp32-processentry32
-        class PROCESSENTRY32(ctypes.Structure):
-            _fields_ = [
-                ("dwSize", ctypes.wintypes.DWORD),
-                ("cntUsage", ctypes.wintypes.DWORD),
-                ("th32ProcessID", ctypes.wintypes.DWORD),
-                ("th32DefaultHeapID", ctypes.POINTER(ctypes.c_ulong)),
-                ("th32ModuleID", ctypes.wintypes.DWORD),
-                ("cntThreads", ctypes.wintypes.DWORD),
-                ("th32ParentProcessID", ctypes.wintypes.DWORD),
-                ("pcPriClassBase", ctypes.c_long),
-                ("dwFlags", ctypes.wintypes.DWORD),
-                ("szExeFile", ctypes.c_char * ctypes.wintypes.MAX_PATH)
-            ]
-
         with CheckWindowsOsError():
             # https://learn.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-createtoolhelp32snapshot
             snapshot = ctypes.windll.kernel32.CreateToolhelp32Snapshot(
@@ -514,7 +217,6 @@ class WindowsProcess(Process):
         return allocation
 
     # TODO: allow other free types
-    # noinspection PyMethodOverriding
     def free_memory(self, address: int):
         with CheckWindowsOsError():
             # https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualfreeex
@@ -601,7 +303,7 @@ class WindowsProcess(Process):
             region_start = module.lpBaseOfDll
             max_size = region_start + module.SizeOfImage
 
-        matches = []
+        matches: list[int] = []
         while region_start < max_size:
             region_info = self.virtual_query(region_start)
             region_start = region_info.BaseAddress + region_info.RegionSize
@@ -744,7 +446,3 @@ class WindowsProcess(Process):
                 return module
 
         raise ValueError(f"No modules named {name}")
-
-# TODO
-# class LinuxProcess(Process):
-#     pass
