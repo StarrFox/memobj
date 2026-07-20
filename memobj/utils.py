b183bee990095b03ba0cb1cf7edad87280dc0785
StarrFox <StarrFox6312@gmail.com>
2026-07-20 13:49:28 -0400

    fix pad_format not including allignment char

--- memobj/utils.py
+++ memobj/utils.py
@@ -149,12 +149,12 @@ def pad_format(format_string: str) -> str:
         stripped_endian = format_string[0]
         format_string = format_string[1:]
 
-    largest = max(struct.calcsize(c) for c in format_string)
+    largest = max(struct.calcsize(stripped_endian + c) for c in format_string)
 
     aligned_format = ""
     offset = 0
     for format_char in format_string:
-        align = struct.calcsize(format_char)
+        align = struct.calcsize(stripped_endian + format_char)
 
         padding = get_padding(offset, align)
         offset += align + padding
